88bdcf2d1504531ef1706009f0ac0dc6432916b5
Marcel O'Neil <marcel@marceloneil.com>
2021-12-23 12:16:32 -0500

    lesson 1: third attempt

--- main.cpp
+++ main.cpp
@@ -1,13 +1,35 @@
 #include "tgaimage.h"
 
+using namespace std;
+
 const TGAColor white = TGAColor(255, 255, 255, 255);
 const TGAColor red = TGAColor(255, 0, 0, 255);
 
 void line(int x0, int y0, int x1, int y1, TGAImage &image, TGAColor color) {
+    bool steep = false;
+
+    // if the line is steep, take it's transpose
+    if (abs(x1 - x0) < abs(y1 - y0)) {
+        steep = true;
+        swap(x0, y0);
+        swap(x1, y1);
+    }
+
+    // make the line left-to-right
+    if (x1 < x0) {
+        swap(x0, x1);
+        swap(y0, y1);
+    }
+
     for (int x = x0; x <= x1; x += 1) {
         float t = (x - x0) / (float) (x1 - x0);
         int y = y0 + (y1 - y0) * t;
-        image.set(x, y, color);
+
+        if (steep) {
+            image.set(y, x, color); // de-transpose
+        } else {
+            image.set(x, y, color);
+        }
     }
 }
 
