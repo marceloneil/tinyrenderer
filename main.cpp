51ed96975cf7e5b9505c10a0befcb95e658bb50b
Marcel O'Neil <marcel@marceloneil.com>
2021-12-23 23:33:43 -0500

    lesson 2: triangles

--- main.cpp
+++ main.cpp
@@ -1,3 +1,4 @@
+#include <algorithm>
 #include <iostream>
 
 #include "model.h"
@@ -7,30 +8,31 @@ using namespace std;
 
 const TGAColor white = TGAColor(255, 255, 255, 255);
 const TGAColor red = TGAColor(255, 0, 0, 255);
+const TGAColor green = TGAColor(0, 255, 0, 255);
+const TGAColor blue = TGAColor(0, 0, 255, 255);
 
-void line(int x0, int y0, int x1, int y1, TGAImage &image, const TGAColor &color) {
+void line(vec2 v0, vec2 v1, TGAImage &image, const TGAColor &color) {
     bool steep = false;
 
     // if the line is steep, take it's transpose
-    if (abs(x1 - x0) < abs(y1 - y0)) {
+    if (abs(v1.x - v0.x) < abs(v1.y - v0.y)) {
         steep = true;
-        swap(x0, y0);
-        swap(x1, y1);
+        swap(v0.x, v0.y);
+        swap(v1.x, v1.y);
     }
 
     // make the line left-to-right
-    if (x1 < x0) {
-        swap(x0, x1);
-        swap(y0, y1);
+    if (v1.x < v0.x) {
+        swap(v0, v1);
     }
 
-    const int dx = x1 - x0;
-    const int dy = y1 - y0;
+    const int dx = v1.x - v0.x;
+    const int dy = v1.y - v0.y;
     const int derror2 = abs(dy) * 2;
 
     int error2 = 0;
-    int y = y0;
-    for (int x = x0; x <= x1; x += 1) {
+    int y = v0.y;
+    for (int x = v0.x; x <= v1.x; x += 1) {
         // if the line is steep, de-transpose
         if (steep) {
             image.set(y, x, color);
@@ -41,59 +43,28 @@ void line(int x0, int y0, int x1, int y1, TGAImage &image, const TGAColor &color
         // slope error calculations
         error2 += derror2;
         if (error2 > dx) {
-            y += y1 > y0 ? 1 : -1;
+            y += v1.y > v0.y ? 1 : -1;
             error2 -= dx * 2;
         }
     }
 }
 
-int main(int argc, char* argv[]) {
-    string filename = "obj/african_head/african_head.obj";
-    int width = 800;
-    int height = 800;
+void triangle(vec2 t0, vec2 t1, vec2 t2, TGAImage &image, const TGAColor &color) {
+    line(t0, t1, image, color);
+    line(t1, t2, image, color);
+    line(t2, t0, image, color);
+}
 
-    try {
-        switch (argc) {
-            case 4:
-                height = stoi(argv[3]);
-                if (height < 1) throw 1;
-                // fall through
-            case 3:
-                width = stoi(argv[2]);
-                if (width < 1) throw 1;
-                // fall through
-            case 2:
-                filename = argv[1];
-                // fall through
-            case 1:
-                break;
-            default:
-                throw 1;
-        }
-    } catch (...) {
-        cerr << "Usage: " << argv[0]
-             << " [ filename [ width (> 0) [ height (> 0) ] ] ]" << endl;
-    }
+int main(int argc __attribute__((unused)), char* argv[] __attribute__((unused))) {
+    TGAImage image(200, 200, TGAImage::RGB);
 
-    Model model(filename);
-    TGAImage image(width, height, TGAImage::RGB);
+    vec2 t0[3] = {vec2(10, 70), vec2(50, 160), vec2(70, 80)};
+    vec2 t1[3] = {vec2(180, 50), vec2(150, 1), vec2(70, 180)};
+    vec2 t2[3] = {vec2(180, 150), vec2(120, 160), vec2(130, 180)};
 
-    for (int face = 0; face < model.nfaces(); face += 1) {
-        // draw a line from each vertex in a face to the next one
-        // there are three vertices in each face
-        for (int vertex = 0; vertex < 3; vertex += 1) {
-            vec3 v0 = model.vert(face, vertex);
-            vec3 v1 = model.vert(face, (vertex + 1) % 3);
-
-            // normalize vertex coordinates which are between -1.0 and 1.0
-            int x0 = (v0.x / 2.0 + 0.5) * width;
-            int y0 = (v0.y / 2.0 + 0.5) * height;
-            int x1 = (v1.x / 2.0 + 0.5) * width;
-            int y1 = (v1.y / 2.0 + 0.5) * height;
-
-            line(x0, y0, x1, y1, image, white);
-        }
-    }
+    triangle(t0[0], t0[1], t0[2], image, red);
+    triangle(t1[0], t1[1], t1[2], image, white);
+    triangle(t2[0], t2[1], t2[2], image, green);
 
     image.write_tga_file("output.tga");
 }
