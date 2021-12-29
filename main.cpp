9720ecdd49b8f885b22bca1b323374cb168c96bc
Marcel O'Neil <marcel@marceloneil.com>
2021-12-29 15:50:18 -0500

    lesson 2: barycentric w/ cross product

--- main.cpp
+++ main.cpp
@@ -49,10 +49,11 @@ void line(vec2 v0, vec2 v1, TGAImage &image, const TGAColor &color) {
     }
 }
 
-void triangle(vec2 t0, vec2 t1, vec2 t2, TGAImage &image, const TGAColor &color) {
+// draw a triangle by filling in lines of pixels between the line segments of the triangle
+void triangle_lineSweep(vec2 t0, vec2 t1, vec2 t2, TGAImage &image, const TGAColor &color) {
     // ignore degenerate triangles
-    if (t0.y == t1.y && t1.y == t2.y) return;
     if (t0.x == t1.x && t1.x == t2.x) return;
+    if (t0.y == t1.y && t1.y == t2.y) return;
 
     // bubble sort vertices by y in ascending order
     vec2 &bottom = t0, &middle = t1, &top = t2;
@@ -100,20 +101,115 @@ void triangle(vec2 t0, vec2 t1, vec2 t2, TGAImage &image, const TGAColor &color)
     }
 }
 
+struct BarycentricCoords {
+    double u, v, w;
+};
+
+// solve for the barycentric coordinates using a cross product
+BarycentricCoords barycentric_crossProduct(vec2 A, vec2 B, vec2 C, vec2 P) {
+    // assume valid triangles
+    assert(A.x != B.x || B.x != C.x);
+    assert(A.y != B.y || B.y != C.y);
+
+    //      (1 - u - v) * A + u * B + v * C = P
+    // <=>  u * (B - A) + v * (C - A) + (A - P) = 0
+    // <=>  u * (B_x - A_x) + v * (C_x - A_x) + (A_x - P_x) = 0
+    //      u * (B_y - A_y) + v * (C_y - A_y) + (A_y - P_y) = 0
+    // <=>  [u, v, 1] * [B_x - A_x, C_x - A_x, A_x - P_x] = 0
+    //      [u, v, 1] * [B_y - A_y, C_y - A_y, A_y - P_y] = 0
+    // <=>  [B_x - A_x, C_x - A_x, A_x - P_x] ⨯ [B_y - A_y, C_y - A_y, A_y - P_y] = [u, v, 1]
+    vec3 result = cross(
+        vec3(B.x - A.x, C.x - A.x, A.x - P.x),
+        vec3(B.y - A.y, C.y - A.y, A.y - P.y)
+    );
+
+    // divide result by some scaling factor 1 * result[2] 
+    double u = result[0] / result[2];
+    double v = result[1] / result[2];
+    double w = 1 - u - v;
+
+    return BarycentricCoords{u, v, w};
+}
+
+inline BarycentricCoords barycentric(vec2 A, vec2 B, vec2 C, vec2 P) {
+#if defined(BARYCENTRIC_CROSSPRODUCT)
+    return barycentric_crossProduct(A, B, C, P);
+#else
+    #error no barycentric method specified
+#endif
+}
+
+// draw a triangle by testing if individual pixels are within the triangle
+void triangle_barycentric(vec2 t0, vec2 t1, vec2 t2, TGAImage &image, const TGAColor &color) {
+    // ignore degenerate triangles
+    if (t0.x == t1.x && t1.x == t2.x) return;
+    if (t0.y == t1.y && t1.y == t2.y) return;
+
+    // find lower left and upper right corners of minimal bounding box
+    vec2 bboxmin(
+        min({t0.x, t1.x, t2.x, image.get_width() - 1.0}),
+        min({t0.y, t1.y, t2.y, image.get_height() - 1.0})
+    );
+    vec2 bboxmax(
+        max({t0.x, t1.x, t2.x, 0.0}),
+        max({t0.y, t1.y, t2.y, 0.0})
+    );
+
+    // clamp bounding box to within image dimensions
+    int xStart = round(max(bboxmin.x, 0.0));
+    int xEnd = round(min(bboxmax.x, image.get_width() - 1.0));
+    int yStart = round(max(bboxmin.y, 0.0));
+    int yEnd = round(min(bboxmax.y, image.get_height() - 1.0));
+
+    // loop through all pixels in bounding box
+    for (int x = xStart; x <= xEnd; x += 1) {
+        for (int y = yStart; y <= yEnd; y += 1) {
+            // check that pixel is within the triangle (simplex)
+            BarycentricCoords coords = barycentric(t0, t1, t2, vec2(x, y));
+            if (coords.u < 0 || coords.v < 0 || coords.w < 0) continue;
+
+            image.set(x, y, color);
+        }
+    }
+}
+
+inline void triangle(vec2 t0, vec2 t1, vec2 t2, TGAImage &image, const TGAColor &color) {
+#if defined(TRIANGLE_LINESWEEP)
+    triangle_lineSweep(t0, t1, t2, image, color);
+#elif defined(TRIANGLE_BARYCENTRIC)
+    triangle_barycentric(t0, t1, t2, image, color);
+#else
+    #error no triangle implementation specified
+#endif
+}
+
 int main(int argc __attribute__((unused)), char* argv[] __attribute__((unused))) {
     TGAImage image(200, 200, TGAImage::RGB);
 
     vec2 t0[3] = {vec2(10, 70), vec2(50, 160), vec2(70, 80)};
     vec2 t1[3] = {vec2(180, 50), vec2(150, 1), vec2(70, 180)};
     vec2 t2[3] = {vec2(180, 150), vec2(120, 160), vec2(130, 180)};
-    vec2 t3[3] = {vec2(25, 25), vec2(50, 50), vec2(75, 25)};
-    vec2 t4[3] = {vec2(55, 50), vec2(80, 25), vec2(105, 50)};
+    vec2 t3[3] = {vec2(25, 25), vec2(30, 50), vec2(75, 25)};
+    vec2 t4[3] = {vec2(65, 60), vec2(105, 15), vec2(110, 60)};
+    vec2 t5[3] = {vec2(160, 120), vec2(250, 140), vec2(250, 50)};
+    vec2 t6[3] = {vec2(-5, 150), vec2(-5, 230), vec2(150, 260)};
+
+    // degenerate triangles
+    vec2 d0[3] = {vec2(5, 5), vec2(5, 5), vec2(5, 5)};
+    vec2 d1[3] = {vec2(5, 5), vec2(100, 5), vec2(150, 5)};
+    vec2 d2[3] = {vec2(5, 5), vec2(5, 100), vec2(5, 150)};
 
     triangle(t0[0], t0[1], t0[2], image, red);
     triangle(t1[0], t1[1], t1[2], image, white);
     triangle(t2[0], t2[1], t2[2], image, green);
     triangle(t3[0], t3[1], t3[2], image, blue);
-    triangle(t4[0], t4[1], t4[2], image, blue);
+    triangle(t4[0], t4[1], t4[2], image, green);
+    triangle(t5[0], t5[1], t5[2], image, red);
+    triangle(t6[0], t6[1], t6[2], image, blue);
+
+    triangle(d0[0], d0[1], d0[2], image, red);
+    triangle(d1[0], d1[1], d1[2], image, red);
+    triangle(d2[0], d2[1], d2[2], image, red);
 
     image.write_tga_file("output.tga");
 }
